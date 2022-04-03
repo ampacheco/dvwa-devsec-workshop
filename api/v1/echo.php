3ed39a450e3585f8446af122589d6fb0a679687a
Robin Wood <robin@digi.ninja>
2022-04-03 11:45:10 +0100

    only check body if POST

--- api/v1/echo.php
+++ api/v1/echo.php
@@ -3,16 +3,18 @@ header ("Content-Type: application/json");
 
 $response = array ("Query" => $_GET);
 
-if ($_SERVER['CONTENT_TYPE'] == "application/json") {
-	$body = file_get_contents('php://input');
-	$decoded = json_decode($body);
-	if (is_null ($decoded)) {
-		$response["Body"] = "Error - Invalid JSON submitted";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	if ($_SERVER['CONTENT_TYPE'] == "application/json") {
+		$body = file_get_contents('php://input');
+		$decoded = json_decode($body);
+		if (is_null ($decoded)) {
+			$response["Body"] = "Error - Invalid JSON submitted";
+		} else {
+			$response["Body"] = $decoded;
+		}
 	} else {
-		$response["Body"] = $decoded;
+		$response["Body"] = "Error - Content type not JSON";
 	}
-} else {
-	$response["Body"] = "Error - Content type not JSON";
 }
 
 echo json_encode ($response);
